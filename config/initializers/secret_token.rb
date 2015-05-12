715205da7f123eb1008a1909a98dabdd0bac9894
srikasip <srikasip@gmail.com>
2015-05-11 23:27:08 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScraperStore::Application.config.secret_key_base = 'ff136f01cd7b18efebf08ad5262a239771182dd844a793a25d2adfe5b478c0ba89f985deedf790a92092d32cc610c1d360e6d8ee205f374c3ab5f82f8407ebf3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScraperStore::Application.config.session_store :cookie_store, key: '_scraper_store_session'
