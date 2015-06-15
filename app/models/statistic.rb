a8ac61f9775b8ec610075110beb3356e8a558417
srikasip <srikasip@gmail.com>
2015-06-15 16:47:37 -0700

    Got te precision output showing on our statistics page

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,2 +1,20 @@
 class Statistic < ActiveRecord::Base
+  def getPrecision(fname)
+    selectStatement = """SELECT count(p.id) as numSamples,
+                          sum(case when p.is_correct='t' then 1 else 0 end) as numCorrect, 
+                          sum(case when p.is_correct='f' then 1 else 0 end) as numWrong, 
+                          avg(p.is_correct='t') as percWrong, 
+                          p.session_id as session_id
+                        FROM precision_inputs as p 
+                        WHERE p.statistic_id = #{self.id} and p.field_name = \"#{fname}\" and (p.is_correct is not NULL)
+                        Group By p.session_id"""
+
+
+    precInputs = PrecisionInput.find_by_sql(selectStatement).delete_if {|item| item["numSamples"]<10}
+    return precInputs
+  end
+
 end
+
+
+
